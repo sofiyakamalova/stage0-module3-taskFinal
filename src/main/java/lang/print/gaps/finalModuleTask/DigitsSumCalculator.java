cb50fcdb70f495d672f308ccc4bb6ed383c8b548
Sophia <116626295+sofiyakamalova@users.noreply.github.com>
2023-02-11 18:01:21 +0600

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int a = number/1000;
+        int b = number/100%10;
+        int c = number/10%10;
+        int d = number%10;
+        int sum = a+b+c+d;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int a = number/100;
+        int b = number/10%10;
+        int c = number%10;
+        int result = c*100 + b*10 + a;
+        System.out.println(result);
     }
 }
